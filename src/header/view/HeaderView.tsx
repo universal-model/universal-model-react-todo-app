6b2fc6286039bdd0b09fe323ab4302b888da87ad
Petri Silen <petri.silen@silensoft.com>
2020-01-28 18:43:41 +0200

    Updated

--- src/header/view/HeaderView.tsx
+++ src/header/view/HeaderView.tsx
@@ -3,12 +3,12 @@ import store from '@/store/store';
 import changeUserName from '@/header/model/actions/changeUserName';
 
 const HeaderView = () => {
-  const { headerState } = store.getState();
-  store.useState([headerState]);
+  const { headerText } = store.getSelectors();
+  store.useSelectors([headerText]);
 
   return (
     <div>
-      <h1>{headerState.userName}</h1>
+      <h1>{headerText.value}</h1>
       <label>User name:</label>
       <input onChange={({ target: { value } }) => changeUserName(value)} />
     </div>
