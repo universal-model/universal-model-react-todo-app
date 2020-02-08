ffc483659ee16f9fee12425747c1556d941eb5bb
Petri Silen <petri.silen@silensoft.com>
2020-02-08 09:47:17 +0200

    Modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
-const path = require("path");
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env = {}) => ({
-  entry: path.resolve(__dirname, "./src/main.tsx"),
+  entry: path.resolve(__dirname, './src/main.tsx'),
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/dist/",
-    filename: "build.js"
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
   },
   resolve: {
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', 'jsx'],
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      '@': path.resolve(__dirname, './src')
     }
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       }
     ]
   },
+  plugins: [
+    // new BundleAnalyzerPlugin()
+  ],
   devServer: {
     inline: true,
     hot: true,
-    stats: "minimal",
+    stats: 'minimal',
     contentBase: __dirname
   }
 });
